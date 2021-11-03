99b4589c07f6d150a43347c6abd7ea8f57f88324
xapamacts <91156866+xapamacts@users.noreply.github.com>
2021-11-03 18:56:10 +0100

    Ready 3

---
+++ Assets/Scripts/General/AppVersion.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AppVersion : MonoBehaviour {
+
+    public Text TextVersion; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        TextVersion.text = "Version: " + Application.version;
+    }
+}
