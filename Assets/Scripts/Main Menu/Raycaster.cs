0eae61703fbc50317365f683052c96f7cf76dd11
xapamacts <91156866+xapamacts@users.noreply.github.com>
2021-11-03 11:24:02 +0100

    Title + Menu

---
+++ Assets/Scripts/Main Menu/Raycaster.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Raycaster : MonoBehaviour
+{
+     public Animator anim;
+     private NavMeshAgent agente;
+
+    void Start()
+    {
+        
+        anim.SetBool("run",false);
+        agente=GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin,ray.direction*100, Color.cyan);
+
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit)== true)
+            {
+                var selection=hit.transform;
+                if(selection.CompareTag("Interactable")){
+                    agente.destination=hit.point;
+                    Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+                    Debug.Log(hit.transform.gameObject.tag);
+                    anim.SetBool("run",true);
+                } 
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            anim.SetBool("run",false);
+        }
+    }
+
+   
+}
