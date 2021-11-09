8ac30a0daed847eaed89a5b85fc73b4917fe894f
xapamacts <91156866+xapamacts@users.noreply.github.com>
2021-11-09 10:32:12 +0100

    Ready classe 4

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Global
+{
+    //Game
+    public static int score = 0;
+    public static int maxScore = 0;
+    public static int level = 0;
+    public static int maxLevel = 10;
+    public static int lives = 3;
+}
